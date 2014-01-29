226bb02ec58e23bb4360c75d6d7f545ae1003fe4
Abraham Xiao <q289196573@gmail.com>
2014-01-30 02:02:07 +0400

    Progress updated. Finish the last part of questions before you are allowed to join the New Year celebrations. Well, someone said something about Dubai? I have no idea.

---
+++ Exam/auto/Notes-Midterm.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "Notes-Midterm"
+ (lambda ()
+    (TeX-run-style-hooks
+     "graphicx"
+     "geometry"
+     "margin=10mm"
+     "amssymb"
+     "amsmath"
+     ""
+     "latex2e"
+     "art10"
+     "article"
+     "twocolumn")))
+
