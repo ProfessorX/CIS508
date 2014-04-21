0ce8a80743bd6e6f0de98a47713b303780d7d6f4
Abraham Xiao <q289196573@gmail.com>
2014-04-21 11:05:53 +0400

    Progress updated. United Arab Emirates or Middle East is a brand new place for people to consider getting their Master's degree. I am honest. Though it is also hard here.

--- Lab/Lab3-3/Droidno.ino
+++ Lab/Lab3-3/Droidno.ino
@@ -28,5 +28,41 @@ void setup() {
 }
 
 void loop() {
-    
+    if (acc.isConnected()) {
+        // read the 2-byte message into the byte array
+        int len = acc.read(rcvmsg, sizeof(rcvmsg), 1);
+        
+        if (len > 0) {
+            if (rcvmsg[0] == COMMAND_LED) {
+                byte value = rcvmsg[1];  // second byte is the actual
+                                         // command sent from phone
+                if (value == LED_ON) {  // if ON command was sent
+                    digitalWrite(LED_PIN, HIGH);
+                }
+                else if (value == LED_OFF) {
+                    digitalWrite(LED_PIN, LOW);
+                }
+            }
+        }
+        
+        // Sample the sensor and send the reading to phone. We'll send
+        // 4-byte integers to demonstrate how to send more than 1-byte
+        // values. We use bit-shift operations.
+        sntmsg[0] = COMMAND_SENSOR;
+        sntmsg[1] = (byte) (sensorValue >> 24);
+        sntmsg[2] = (byte) (sensorValue >> 16);
+        sntmsg[3] = (byte) (sensorValue >> 8);
+        sntmsg[4] = (byte) (sensorValue);
+
+        // send the constructed 5-byte message to the phone
+        acc.write(sntmsg, 5);
+
+        // arbitrarily change dummy sensor in each iteration of loop
+        // so that sensor readings can be observed changing in
+        // real-time through Android APPs.
+        sensorValue += 10;
+        if (sensorValue > 500) {
+            sensorValue = 100;  // reset
+        }
+    }
 }
