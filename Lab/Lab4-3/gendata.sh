33ce5983c73d6496bdba94bdebcdc8a0c738b810
Abraham Xiao <q289196573@Gmail.com>
2014-05-14 02:58:16 +0400

    All about mathematics. Are you ready for this? To be or not to be, no third option in the United Arab Emirates as a researcher.
    "Come on, see how hard-working your fellows are. Do not be a nob and Do
    fucking nothing!!!"

--- Lab/Lab4-3/gendata.sh
+++ Lab/Lab4-3/gendata.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    Adafruit_DHT 22 17 | sed -e '1\t2d' -e 's/[\ \*=\%a-zA-Z]//g'
+    Adafruit_DHT 22 17 | sed -e '1,2d' -e 's/[\ \*=\%a-zA-Z]//g' -e 's/,/'
     if [! -z "$vals" ]
         then 
         echo 'date + "%Y-%m-%d|%H:%M:%S"' $vals
